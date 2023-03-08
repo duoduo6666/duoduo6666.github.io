b045328860f0f1b3f3c27899553dd777e7e1a5ad
duoduo6666 <2326064185@qq.com>
2023-03-08 22:00:57 +0000

    支持了Google Analytics

--- docs/笔记/安装 SSH 服务端 (OpenSSH).md
+++ docs/笔记/安装 SSH 服务端 (OpenSSH).md
@@ -1,8 +1,5 @@
 # 安装 SSH 服务端 (OpenSSH)
-<div style="font-size:10px; line-height:1px">
-<p>本文作者： <a href="{site_author_url}">{site_author}</a>, 文章链接： <a href="{site_url}{url}">{site_url}{url_decode}</a></p>
-    <p>本文为博主原创文章，采用<strong><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></strong>协议发布。</p>
-</div>
+{state}
 
 ## 安装
 
--- my_hooks.py
+++ my_hooks.py
@@ -1,12 +1,14 @@
 from urllib.parse import unquote
 
+site_author_url = "https://github.com/duoduo6666/"
+
 def on_page_markdown(markdown:str, **kwargs):
-    print(kwargs)
-    k = {
-        "site_author_url": "https://github.com/duoduo6666/",
-        "site_author": kwargs["config"]["site_author"],
-        "site_url": kwargs["config"]["site_url"],
-        "url": kwargs["page"].url,
-        "url_decode": unquote(kwargs["page"].url)
-        }
-    return markdown.format(**k)
+    site_author =  kwargs["config"]["site_author"]
+    site_url = kwargs["config"]["site_url"]
+    url = kwargs["page"].url
+    url_decode = unquote(kwargs["page"].url)
+    state = f"""<div style="font-size:10px; line-height:1px">
+<p>本文作者： <a href="{site_author_url}">{site_author}</a>, 文章链接： <a href="{site_url}{url}">{site_url}{url_decode}</a></p>
+<p>本文为博主原创文章，采用<strong><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></strong>协议发布。</p>
+</div>"""
+    return markdown.format(state=state)
